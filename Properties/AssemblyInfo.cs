fd536337a5cfe1874e18e1186d63f61882f39cc8
Ernst Peter Tamminga <ernst.peter.tamminga@xcess.nl>
2018-05-28 11:17:33 +0200

    First level conversion VB>net to C#, implementing DNNtc.MSBuild structure. Solution does not build yet, need C# fixing and cleanup (MArk-89)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,5 @@
 // VBConversions Note: VB project level imports
-using System.Web.UI.WebControls;
-using DotNetNuke.Services.Exceptions;
-using DotNetNuke.Common.Utilities;
-using DotNetNuke.Entities.Tabs;
-using System.Text.RegularExpressions;
 using System;
-using DotNetNuke.Entities;
-using DotNetNuke.Framework;
-using DotNetNuke;
-using System.Text;
-using System.Web.UI.HtmlControls;
-using DotNetNuke.Services;
-using DotNetNuke.Common;
-using DotNetNuke.UI;
-using DotNetNuke.Data;
-using System.Collections;
-using System.Web.Profile;
-using System.Collections.Specialized;
-using Microsoft.VisualBasic;
-using System.Diagnostics;
-using DotNetNuke.Security;
-using System.Web.UI;
-using System.Web.SessionState;
-using System.Data;
-using System.Configuration;
-using System.Web;
-using DotNetNuke.Services.Localization;
-using System.Web.Security;
-using DotNetNuke.Modules;
-using System.Web.Caching;
-using System.Web.UI.WebControls.WebParts;
-using System.Collections.Generic;
 // End of VB project level imports
 
 using System.Reflection;
@@ -43,17 +12,17 @@ using System.Runtime.InteropServices;
 
 // Review the values of the assembly attributes
 
-[assembly:AssemblyTitle("IdentitySwitcher")]
-[assembly:AssemblyDescription("IdentitySwitcher is a module that allows for easy switching between different portal identities. This is extremely usefull during site development. NB:using this module on your own site might have security implications. Please think before you act!")]
+[assembly: AssemblyTitle("IdentitySwitcher")]
+[assembly: AssemblyDescription("IdentitySwitcher is a module that allows for easy switching between different portal identities. This is extremely usefull during site development. NB:using this module on your own site might have security implications. Please think before you act!")]
 [assembly: AssemblyCompany("dnn-connect.org")]
-[assembly:AssemblyProduct("IdentitySwitcher")]
+[assembly: AssemblyProduct("IdentitySwitcher")]
 [assembly: AssemblyCopyright("Copyright 2018")]
 [assembly: AssemblyTrademark("www.dnn-connect.org")]
 
-[assembly:ComVisible(false)]
+[assembly: ComVisible(false)]
 
 //The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly:Guid("e32aac77-59ca-4e1a-b422-44c309552c55")]
+[assembly: Guid("e32aac77-59ca-4e1a-b422-44c309552c55")]
 
 // Version information for an assembly consists of the following four values:
 //
