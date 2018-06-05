04a74b13e408cd1a6a8d723b0fe5c28c487f693b
Mark van de Vries <mark.van.de.vries@xcess.nl>
2018-06-05 17:31:10 +0200

    Initial setup for frontend + webapi calls

---
+++ Components/IdentitySwitcherController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DNN.Modules.IdentitySwitcher.Components
+{
+    using System.Web.Http;
+    using DotNetNuke.Common.Utilities;
+    using DotNetNuke.Entities.Users;
+    using DotNetNuke.Security;
+    using DotNetNuke.Web.Api;
+
+    public class IdentitySwitcherController: DnnApiController
+    {
+        [DnnAuthorize]
+        [HttpGet]
+        public IHttpActionResult SwitchUser()
+        {
+            var User = 2;
+            var PortalId = 0;
+            var IdentityName = "host";
+            var SelectedValue = "2";
+            var UserHostAddress = "127.0.0.1";
+
+            var MyUserInfo = UserController.GetUserById(PortalId, int.Parse(SelectedValue));
+
+            if (User != null)
+            {
+                DataCache.ClearUserCache(this.PortalSettings.PortalId, IdentityName);
+            }
+
+            // sign current user out
+            var objPortalSecurity = new PortalSecurity();
+            objPortalSecurity.SignOut();
+
+            // sign new user in
+            UserController.UserLogin(PortalId, MyUserInfo, this.PortalSettings.PortalName,
+                                     UserHostAddress, false);
+
+            return this.Ok();
+        }
+    }
+}
---
+++ Components/RouteMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DNN.Modules.IdentitySwitcher.Components
+{
+    using System.Web.Http.Controllers;
+    using DotNetNuke.Framework.Reflections;
+    using DotNetNuke.Web.Api;
+    public class RouteMapper: IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute("Identity Switcher", "default", "{controller}/{action}", new[] { "DNN.Modules.IdentitySwitcher.Components" });
+        }
+
+    }
+}
