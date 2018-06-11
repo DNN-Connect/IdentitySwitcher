214c27fd1d2d77a169801be8fa24870d6dd2adf6
Mark van de Vries <mark.van.de.vries@xcess.nl>
2018-06-11 14:32:05 +0200

    Added UserDto

--- Components/IdentitySwitcherController.cs
+++ Components/IdentitySwitcherController.cs
@@ -10,23 +10,38 @@ namespace DNN.Modules.IdentitySwitcher.Components
     using DotNetNuke.Entities.Profile;
     using DotNetNuke.Entities.Users;
     using DotNetNuke.Security;
+    using DotNetNuke.Security.Roles;
     using DotNetNuke.Web.Api;
 
     public class IdentitySwitcherController: DnnApiController
     {
-        //private SortBy SortResultsBy
-        //{
-        //    get
-        //        {
-        //            var bRetValue = SortBy.DisplayName;
-        //            if (this.Settings.Contains("sortBy"))
-        //            {
-        //                bRetValue = (SortBy)Enum.Parse(typeof(SortBy),
-        //                                               Convert.ToString(this.Settings["sortBy"].ToString()));
-        //            }
-        //            return bRetValue;
-        //        }
-        //}
+        private IEnumerable<UserInfo> Users {get; set; }
+
+        private SortBy SortResultsBy
+        {
+            get
+            {
+                var bRetValue = SortBy.DisplayName;
+
+                var moduleId = this.ActiveModule.ModuleID;
+
+                var repository = new IdentitySwitcherModuleSettingsRepository();
+                var settings = repository.GetSettings(this.ActiveModule);
+
+                if (settings.SortBy != null)
+                {
+                    bRetValue = (SortBy)Enum.Parse(typeof(SortBy),
+                                                   Convert.ToString(settings.SortBy));
+                }
+
+                //if (this.Settings.Contains("sortBy"))
+                //{
+                //    bRetValue = (SortBy)Enum.Parse(typeof(SortBy),
+                //                                   Convert.ToString(this.Settings["sortBy"].ToString()));
+                //}
+                return bRetValue;
+            }
+        }
 
 
         [DnnAuthorize]
@@ -77,54 +92,93 @@ namespace DNN.Modules.IdentitySwitcher.Components
         [HttpGet]
         public IHttpActionResult GetUsers(string searchText, string selectedSearchItem)
         {
-            if (searchText == "")
+            if (searchText == null)
             {
                 this.LoadAllUsers();
             }
             else
             {
-                //this.Filter(searchText, selectedSearchItem);
+                this.Filter(searchText, selectedSearchItem);
             }
 
+            var result = new List<UserDto>();
+
+            foreach (UserInfo userInfo in this.Users)
+            {
+                var dto = new UserDto
+                              {
+                                  Name = userInfo.Username,
+                                  Id = userInfo.UserID
+                              };
+                result.Add(dto);
+            }
+
+
             //return this.Ok(result);
-            return null;
+            return this.Ok(result);
         }
 
         private void LoadAllUsers()
         {
-            //var users = UserController.GetUsers(this.PortalId).OfType<UserInfo>().ToList();
-            //this.BindUsers(users);
+            this.Users = UserController.GetUsers(this.PortalSettings.PortalId).OfType<UserInfo>().ToList();
+            this.SortUsers();
+        }
+
+        private void SortUsers()
+        {
+            switch (this.SortResultsBy)
+            {
+                case SortBy.DisplayName:
+                    this.Users = this.Users.OrderBy(arg => arg.DisplayName.ToLower());
+                    break;
+                case SortBy.UserName:
+                    this.Users = this.Users.OrderBy(arg => arg.Username.ToLower());
+                    break;
+            }
+
+            //var display = "";
+            //foreach (var user in users)
+            //{
+            //    if (this.SortResultsBy == SortBy.DisplayName)
+            //    {
+            //        display = string.Format("{0} - {1}", user.DisplayName, user.Username);
+            //    }
+            //    else
+            //    {
+            //        display = string.Format("{0} - {1}", user.Username, user.DisplayName);
+            //    }
+            //    //this.cboUsers.Items.Add(new ListItem(display, user.UserID.ToString()));
+            //}
+        }
+
+        private void Filter(string searchText, string selectedSearchItem)
+        {
+            //var users = default(List<UserInfo>);
+            var total = 0;
+
+            switch (selectedSearchItem)
+            {
+                case "Email":
+                    this.Users = UserController.GetUsersByEmail(this.PortalSettings.PortalId, searchText + "%", -1, -1, ref total)
+                                          .OfType<UserInfo>().ToList();
+                    break;
+                case "Username":
+                    this.Users = UserController.GetUsersByUserName(this.PortalSettings.PortalId, searchText + "%", -1, -1, ref total)
+                                          .OfType<UserInfo>().ToList();
+                    break;
+                case "RoleName":
+                    this.Users = RoleController.Instance.GetUsersByRole(this.PortalSettings.PortalId, searchText).ToList();
+                    break;
+
+                default:
+                    this.Users = UserController
+                        .GetUsersByProfileProperty(this.PortalSettings.PortalId, selectedSearchItem, searchText + "%", 0, 1000, ref total)
+                        .OfType<UserInfo>().ToList();
+                    break;
+            }
+            this.SortUsers();
 
             //this.LoadDefaultUsers();
         }
-
-        //private void BindUsers(IEnumerable<UserInfo> users)
-        //{
-        //    //this.cboUsers.Items.Clear();
-
-        //    switch (this.SortResultsBy)
-        //    {
-        //        case SortBy.DisplayName:
-        //            users = users.OrderBy(arg => arg.DisplayName.ToLower());
-        //            break;
-        //        case SortBy.UserName:
-        //            users = users.OrderBy(arg => arg.Username.ToLower());
-        //            break;
-        //    }
-
-        //    var display = "";
-        //    foreach (var user in users)
-        //    {
-        //        if (this.SortResultsBy == SortBy.DisplayName)
-        //        {
-        //            display = string.Format("{0} - {1}", user.DisplayName, user.Username);
-        //        }
-        //        else
-        //        {
-        //            display = string.Format("{0} - {1}", user.Username, user.DisplayName);
-        //        }
-        //        this.cboUsers.Items.Add(new ListItem(display, user.UserID.ToString()));
-        //    }
-        //}
     }
 }
---
+++ Components/UserDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DNN.Modules.IdentitySwitcher.Components
+{
+    using Newtonsoft.Json;
+
+    public class UserDto
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
